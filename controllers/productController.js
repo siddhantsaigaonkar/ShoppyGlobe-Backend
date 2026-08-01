f8a566466bc1ede7bbd71a73d2daf12b8074cc3b
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-08-01 13:03:02 +0530

    Add product model, controller, and routes

---
+++ controllers/productController.js
@@ -0,0 +1,43 @@
+import Product from "../models/Product.js";
+
+// GET /products
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      data: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// GET /products/:id
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+
 
 dotenv.config();
 
@@ -11,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/products", productRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
