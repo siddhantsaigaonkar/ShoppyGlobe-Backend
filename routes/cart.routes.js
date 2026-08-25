63fae18244acd4f3176d7a692db7e90fbdc36e0e
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-08-25 19:19:43 +0530

    Add protected cart routes

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,14 +1,28 @@
 import express from "express";
-import { addToCart,updateCart,removeFromCart } from "../Controller/cart.controller.js";
 
+import {
+  addToCart,
+  updateCart,
+  removeFromCart,
+  getCart
+} from "../Controller/cart.controller.js";
+
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+
+// Protected route - only logged-in users can view their cart
+router.get("/", authMiddleware, getCart);
+
+
 // POST /api/cart
-router.post("/", addToCart);
+router.post("/", authMiddleware, addToCart);
+
 // PUT /api/cart/:id
-router.put("/:id", updateCart);
+router.put("/:id", authMiddleware, updateCart);
+
 // DELETE /api/cart/:id
-router.delete("/:id", removeFromCart);
+router.delete("/:id", authMiddleware, removeFromCart);
 
 export default router;
